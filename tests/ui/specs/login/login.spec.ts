bc93725c288f4e6303fc982d58ce423bd6b3999e
Alexandre <alexandrefrei@gmail.com>
2025-10-31 11:46:58 -0300

    Update login.spec.ts

--- tests/ui/specs/login/login.spec.ts
+++ tests/ui/specs/login/login.spec.ts
@@ -5,6 +5,8 @@ import LoginPage from '../../pages/loginPage';
 let loginPage: LoginPage;
 
 test.beforeEach(async ({ page }) => {
+  await allure.parentSuite('Webhook OpenAI Tests')
+  await allure.suite('Core Functionality')
   await page.goto('/');
   // await page.goto('https://admin.cordial.io/#/login');
 
