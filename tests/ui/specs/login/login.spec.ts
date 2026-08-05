eb6ca22adb37ae028a969d3afaa8baa2063c5c73
Alexandre <alexandrefrei@gmail.com>
2026-08-05 16:39:10 -0300

    Update login.spec.ts

--- tests/ui/specs/login/login.spec.ts
+++ tests/ui/specs/login/login.spec.ts
@@ -18,7 +18,7 @@ test.beforeEach(async ({ page }) => {
 test.describe('Login Page', () => {
   test('Verify the login page with valid email and password', {tag: '@smoke' } , async () => {
     console.log(` *** Environment: ${process.env.TEST_ENV!}`);
-    console.log(` ### Username: ${process.env.USERNAME!} `);
+    console.log(` ### Username: ${process.env.USERNAME} `);
     console.log(` @@@@ User: ${USERNAME} `);
     //const baseURL = test.info().project.use.baseURL!; //How to access the baseURL inside the test
     await loginPage.loginUI(USERNAME, PASSWORD);
