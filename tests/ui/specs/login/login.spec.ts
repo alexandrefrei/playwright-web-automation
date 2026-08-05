6524fbe6af7a9f4565285868e1d6f2ffc7cb4be5
Alexandre <alexandrefrei@gmail.com>
2026-08-05 15:52:54 -0300

    Update login.spec.ts

--- tests/ui/specs/login/login.spec.ts
+++ tests/ui/specs/login/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
-import { USER_EMAIL, PASSWORD } from '../../../support/utils/config';
 import LoginPage from '../../pages/loginPage';
+import { getCredentials } from '../../../support/utils/settingsConfig';
 import * as allure from 'allure-js-commons'
 
 let loginPage: LoginPage;
@@ -8,29 +8,30 @@ let loginPage: LoginPage;
 test.beforeEach(async ({ page }) => {
   await allure.parentSuite('Login')
   await page.goto('/');
-  // await page.goto('https://admin.cordial.io/#/login');
+ ({ username, password } = getCredentials());
 
   loginPage = new LoginPage(page);
 });
 
 //login
 
-test.describe('Login Page @smokeTest', () => {
-  test('Verify the login page with valid email and password', async () => {
+test.describe('Login Page', () => {
+  test('Verify the login page with valid email and password', {tag: '@smoke' } , async () => {
     console.log(` *** Environment: ${process.env.ENVIRONMENT!}`);
+    console.log(` *** Username: username `);
     //const baseURL = test.info().project.use.baseURL!; //How to access the baseURL inside the test
 
     await test.step('Log in', async () => {
-      await loginPage.loginUI(USER_EMAIL, PASSWORD);
+      await loginPage.loginUI(username, password);
     });
   });
 
   test('Verify the login page with invalid email', async ({ page }) => {
-    await loginPage.loginUI('invalid', PASSWORD);
+    await loginPage.loginUI('invalid', password);
     await loginPage.checkInvalidCredentials();
   });
   test('Verify the login page with invalid password', async ({ page }) => {
-    await loginPage.loginUI(USER_EMAIL, 'invalid');
+    await loginPage.loginUI(username, 'invalid');
     await loginPage.checkInvalidCredentials();
   });
 });
